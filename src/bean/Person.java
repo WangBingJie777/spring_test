85874fbc27d3c85910063a79113e4ebf20cb02a1
Wangbj <Wangbi@atguigu.com>
2022-11-28 21:25:53 +0800

    second commit

--- src/bean/Person.java
+++ src/bean/Person.java
@@ -5,4 +5,14 @@ package bean;
  * @create 2022-11-28 20:32
  */
 public class Person {
+    private String name;
+    private String age;
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age='" + age + '\'' +
+                '}';
+    }
 }
